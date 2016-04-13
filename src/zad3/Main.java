e9e292185e5a87024e711b095899abe1ab8a6839
majki <michal.m.baran@gmail.com>
2016-04-13 21:29:34 +0200

    Initial commit

---
+++ src/zad3/Main.java
@@ -0,0 +1,24 @@
+/**
+ * @author Baran Michał PD2067
+ */
+
+package zad3;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        printTest();
+    }
+
+    private static void printTest() {
+        System.out.println("Moday");
+        System.out.println("Moday");
+        System.out.println("Moday");
+        System.out.println("Moday");
+        System.out.println("Moday");
+        System.out.println("Moday");
+
+
+    }
+}
---
+++ src/zad4/Main.java
@@ -0,0 +1,14 @@
+/**
+ *
+ *  @author Baran Michał PD2067
+ *
+ */
+
+package zad4;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+  }
+}

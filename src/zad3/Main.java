3c8283abffc05e7925e1a86f7e548aafab7fbde6
majki <michal.m.baran@gmail.com>
2016-04-13 22:33:31 +0200

    reset

--- src/zad3/Main.java
+++ src/zad3/Main.java
@@ -8,17 +8,7 @@ package zad3;
 public class Main {
 
     public static void main(String[] args) {
-        printTest();
-    }
-
-    private static void printTest() {
-        System.out.println("Saturday");
-        System.out.println("Saturday");
-        System.out.println("Saturday");
-        System.out.println("Saturday");
-        System.out.println("Saturday");
-        System.out.println("Saturday");
-
 
     }
+
 }

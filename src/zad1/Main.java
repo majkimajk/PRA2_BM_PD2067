bfe6f1947d92e97ad1a26fe7f494c4ba6e5b18b9
majki <michal.m.baran@gmail.com>
2016-04-23 00:01:25 +0200

    first try

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -1,14 +1,64 @@
 /**
- *
- *  @author Baran Michał PD2067
- *
+ * @author Baran Michał PD2067
  */
 
 package zad1;
 
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.SwingConstants.*;
 
-  public static void main(String[] args) {
-  }
+public class Main{
+
+    final int NUMOFET = 5;
+
+
+    Color backCol[] = { new Color(191, 225, 255), new Color(255, 255, 200),
+            new Color(201, 245, 245), new Color(255, 255, 140),
+            new Color(161, 224, 224), new Color(255, 255, 200), };
+    Color fontCol[] = { new Color(191, 225, 255), new Color(255, 255, 200),
+            new Color(201, 245, 245), new Color(255, 255, 140),
+            new Color(161, 224, 224), new Color(255, 255, 200), };
+  /*  String[] backCol = {"BLACK","RED","YELLOW","BLUE","GREEN"};
+    String[] fontCol = {"RED","YELLOW","BLACK","GREEN","BLUE"};*/
+  String place[] = { "West", "North", "East", "South", "Center" };
+    int[] fontSize = {12, 21, 10, 8, 30, 14};
+    String[] texts = {"Jeden","Dwa","Trzy","Cztery","Pięć"};
+    String[] tips = {"One","Two","Three","Four","Five"};
+
+
+    Main() {
+
+        JFrame f = new JFrame("Etykiety");
+        f.setLayout(new BorderLayout());
+
+        for (int i = 0; i < NUMOFET; i++) {
+
+            JLabel l = new JLabel();
+           // l.setBackground(backCol[i]);
+            l.setFont(new Font("aaa", Font.ITALIC, 30));
+            l.setText(texts[i]);
+            l.setToolTipText(tips[i]);
+            l.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            f.add(l, place[i]);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.pack();
+            f.setVisible(true);
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+
+    }
 }
--- src/zad2/Main.java
+++ src/zad2/Main.java
@@ -10,5 +10,7 @@ package zad2;
 public class Main {
 
   public static void main(String[] args) {
+
+    System.out.println("sdsds");
   }
 }

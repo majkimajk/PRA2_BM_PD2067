9cd0e1e8160b005e0fd55c94965328e379728daf
majki <michal.m.baran@gmail.com>
2016-04-26 00:01:02 +0200

    zad 3 done, drugie ciagle do sprawdzenia

--- src/zad3/Main.java
+++ src/zad3/Main.java
@@ -5,10 +5,73 @@
 package zad3;
 
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    Color[] colors = {
+            Color.MAGENTA,
+            Color.RED,
+            Color.GREEN,
+            Color.CYAN,
+            Color.ORANGE
+    };
+
+    int licznik = 0;
+
+
+    public Main() {
+
+        JButton b = new JButton();
+        b.setPreferredSize(new Dimension(700, 700));
+
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                b.setBackground(colors[licznik]);
+                licznik++;
+
+                if (licznik == colors.length) {
+                    licznik = 0;
+                }
+                ;
+            }
+
+        });
+
+        JFrame f = new JFrame();
+        f.add(b);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.pack();
+
 
     }
 
+    public static void main(String[] args) {
+
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+
+    }
+
+
+/*    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        b.setBackground(Color.RED);
+
+    }*/
 }
